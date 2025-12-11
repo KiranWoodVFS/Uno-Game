71e4aab23952b22c8ad5ea704299b870343bab1e
Tobias <pg29tobias@vfs.com>
2025-12-11 10:38:39 -0800

    init tobi1

--- uno-game/uno-game/GameManager.cpp
+++ uno-game/uno-game/GameManager.cpp
@@ -1,1 +1,25 @@
 #include "GameManager.h"
+
+GameManager* GameManager::sInstance = nullptr;
+
+GameManager* GameManager::GetInstance()
+{
+    if (sInstance == nullptr)
+    {
+        sInstance = new GameManager();
+    }
+    return sInstance;
+}
+
+GameManager::GameManager()
+{
+}
+
+GameManager::~GameManager()
+{
+}
+
+void GameManager::Run()
+{
+
+}
--- uno-game/uno-game/GameManager.h
+++ uno-game/uno-game/GameManager.h
@@ -1,7 +1,14 @@
 #pragma once
+
 class GameManager
 {
 	// Calls the functions from other classes
 	// manages if the game has been won
+
+public:
+	static GameManager* GetInstance();
+	void Run();
+private:
+	static GameManager* sInstance;
 };
 
